d40cc84caabc440dda3d82ddfe18df10d0bcdf09
gingemonster <brian@earthware.co.uk>
2019-12-01 13:54:17 +0000

    day 1

---
+++ 1/a/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Aoc
+{
+    using System;
+    using System.IO;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var totalfuel = 0d;
+            foreach (var line in File.ReadLines(args[0]))
+            {
+                totalfuel += Math.Floor((double)(int.Parse(line) / 3)) - 2;
+            }
+
+            Console.WriteLine(totalfuel);
+        }
+    }
+}
---
+++ 1/b/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Aoc
+{
+    using System;
+    using System.IO;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var totalfuel = 0d;
+            foreach (var line in File.ReadLines(args[0]))
+            {
+                var modulefuel = Math.Floor((double)(int.Parse(line) / 3)) - 2;
+
+                // calculate fuel for fuel
+                var lastfuel = modulefuel;
+                while (true)
+                {
+                    var fuelfuel = Math.Floor((double)(lastfuel / 3)) - 2;
+                    if (fuelfuel > 0)
+                    {
+                        modulefuel += fuelfuel;
+                        lastfuel = fuelfuel;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                totalfuel += modulefuel;
+            }
+
+            Console.WriteLine(totalfuel);
+        }
+    }
+}

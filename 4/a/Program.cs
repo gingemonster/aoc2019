bd6a238870172af2208105eb847f3830145153d1
gingemonster <brian@earthware.co.uk>
2019-12-04 18:24:03 +0000

    day 4 part 2 not great optimization

---
+++ 4/a/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Aoc
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var matches = 0;
+            for (var i = 240920; i <= 789857; i++)
+            {
+                if (Matches(i.ToString()))
+                {
+                    Console.WriteLine(i);
+                    matches++;
+                }
+            }
+
+            Console.WriteLine(matches);
+        }
+
+        private static bool Matches(string number)
+        {
+            var repeatedregex = @"(\w)\1+";
+            if (number.Length != 6)
+            {
+                return false;
+            }
+            
+            if (!Regex.IsMatch(number, repeatedregex))
+            {
+                return false;
+            }
+
+            var chars = number.ToCharArray();
+            for (var i = 1; i < chars.Length; i++)
+            {
+                if (int.Parse(chars[i].ToString()) < int.Parse(chars[i - 1].ToString()))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}

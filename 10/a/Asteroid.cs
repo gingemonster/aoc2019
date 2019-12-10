cf18c3a2e4cb0d6e82dd2668eef95458a09e7fb3
Brian Norman <brian@earthware.co.uk>
2019-12-10 15:56:16 +0000

    day 10

---
+++ 10/a/Asteroid.cs
@@ -0,0 +1,31 @@
+﻿namespace Aoc
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Asteroid
+    {
+        private List<Tuple<int, double>> otherAsteroids = new List<Tuple<int, double>>();
+
+        public int Id
+        {
+            get; set;
+        }
+
+        public int X
+        {
+            get; set;
+        }
+
+        public int Y
+        {
+            get; set;
+        }
+
+        public List<Tuple<int, double>> OtherAsteroids
+        {
+            get => this.otherAsteroids; set => this.otherAsteroids = value;
+        }
+    }
+}
---
+++ 10/a/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Aoc
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var asteroids = new List<Asteroid>();
+            var lines = File.ReadLines(args[0]).ToArray();
+            var numcolumns = lines.First().Length;
+            var i = 0;
+
+            for (var y = 0; y < lines.Count(); y++)
+            {
+                for (var x = 0; x < numcolumns; x++)
+                {
+                    if (lines[y][x] == '#')
+                    {
+                        asteroids.Add(new Asteroid() { X = x, Y = y, Id = i });
+                        i++;
+                    }
+                }
+            }
+
+            asteroids.ForEach(a =>
+            {
+                asteroids.Where(o => o.Id != a.Id).ToList().ForEach(o =>
+                {
+                    var xdiff = o.X - a.X;
+                    var ydiff = o.Y - a.Y;
+                    var anglebetween = Math.Atan2(xdiff, ydiff);
+
+                    a.OtherAsteroids.Add(new Tuple<int, double>(o.Id, anglebetween));
+                });
+            });
+
+            var best = asteroids.OrderByDescending(a => a.OtherAsteroids.GroupBy(o => o.Item2).Count()).First();
+        }
+    }
+}

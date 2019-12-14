cd0de84afea482d5018db94776af718ef71f8226
gingemonster <brian@earthware.co.uk>
2019-12-14 10:25:12 +0000

    day 15 part 1 actually works at last!

--- 15/a/Program.cs
+++ 15/a/Program.cs
@@ -18,11 +18,86 @@
             var ingredienceneeded = new List<Ingredient>();
             var spareingredience = new List<Ingredient>();
 
+            // work back to find all ingrediant and quanity needed for 1 fuel
             while (true)
             {
                 var reaction = FindFirstReactionWithNoDependants(reactions);
                 reactions.Remove(reaction);
+
+                if (reaction.Output.Item1 == "FUEL")
+                {
+                    ingredienceneeded.Add(new Ingredient("FUEL", 1));
+                }
+
+                // take first needed
+                var needed = ingredienceneeded.Where(i => i.Name == reaction.Output.Item1).First();
+                ingredienceneeded.Remove(needed);
+
+                // get its parts
+                GetIngredienceFromReaction(reaction, ingredienceneeded, spareingredience, needed.Quantity);
+
+                if (ingredienceneeded.Count == 1)
+                {
+                    break;
+                }
             }
+
+            var remaining = ingredienceneeded[0];
+            Console.WriteLine($"{remaining.Quantity} {remaining.Name}");
+        }
+
+        private static void GetIngredienceFromReaction(Reaction reaction, List<Ingredient> partsneeded, List<Ingredient> spareingredients, int quantityneeded)
+        {
+            // work out how many times the partsneeded
+            var multiples = (int)Math.Ceiling(quantityneeded / (double)reaction.Output.Item2);
+
+            // work out how much spare we would generate
+            var sparegenerated = reaction.Output.Item2 - quantityneeded;
+
+            if (sparegenerated > 0 && spareingredients.Any(s => s.Name == reaction.Output.Item1))
+            {
+                spareingredients.Where(s => s.Name == reaction.Output.Item1).First().Quantity += sparegenerated;
+            }
+            else if (sparegenerated > 0)
+            {
+                spareingredients.Add(new Ingredient(reaction.Output.Item1, sparegenerated));
+            }
+
+            reaction.Inputs.ForEach(i =>
+            {
+                var needed = i.Quantity * multiples;
+
+                // check spare ingredients to see if we need any more
+                var numberspare = spareingredients.Where(s => s.Name == i.Name).Any() ? spareingredients.Where(i => i.Name == i.Name).FirstOrDefault().Quantity : 0;
+
+                if (numberspare > 0)
+                {
+                    // there are more spare than we need
+                    if (numberspare >= needed)
+                    {
+                        // decrease the number of spares
+                        spareingredients.Where(s => s.Name == i.Name).First().Quantity -= quantityneeded;
+                        needed = 0;
+                    }
+                    else
+                    {
+                        // there are less spare than we need
+                        needed -= numberspare;
+
+                        // remove spares as we have none left (if there were any spare to start with)
+                        spareingredients.RemoveAll(s => s.Name == i.Name);
+                    }
+                }
+
+                if (partsneeded.Any(x => x.Name == i.Name))
+                {
+                    partsneeded.Where(x => x.Name == i.Name).FirstOrDefault().Quantity += needed;
+                }
+                else
+                {
+                    partsneeded.Add(new Ingredient(i.Name, needed));
+                }
+            });
         }
 
         private static Reaction FindFirstReactionWithNoDependants(List<Reaction> reactions)
